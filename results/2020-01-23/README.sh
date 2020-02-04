0bbfaa274468cb120c9b0a093476fe72dc446bc7
ignasi <ignasilucas@gmail.com>
2020-02-04 20:27:01 +0100

    Included results from the original annotation with interproscan, to look at gene functions with more detail.

--- results/2020-01-23/README.sh
+++ results/2020-01-23/README.sh
@@ -3,6 +3,13 @@
 #				2020-01-23
 #				----------
 #
+TRANSCRIPTS='../2019-07-10/transcripts.fa'
+
+# transcript2gene is a space separated text file with two columns: transcript
+# and gene names.
+if [ ! -e transcript2gene.txt ]; then
+   grep "^>" $TRANSCRIPTS | sed -E 's/^>|gene_id://g' > transcript2gene.txt
+fi
 
 if [ ! -e interpretation.html ]; then
    R --save -q -e "rmarkdown::render('interpretation.Rmd', output_file='interpretation.html')"
